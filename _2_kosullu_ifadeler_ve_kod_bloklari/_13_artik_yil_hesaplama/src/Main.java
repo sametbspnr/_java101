9b7b05385ae2dd9d7960b41e6283d345c5bbf7a4
Samet <sametbasp@gmail.com>
2024-02-26 22:28:53 +0300

    _13_artik_yil_hesaplama_eklendi

--- _2_kosullu_ifadeler_ve_kod_bloklari/_13_artik_yil_hesaplama/src/Main.java
+++ _2_kosullu_ifadeler_ve_kod_bloklari/_13_artik_yil_hesaplama/src/Main.java
@@ -9,10 +9,8 @@ public class Main {
         System.out.println("Yıl Giriniz: ");
         year = input.nextInt();
 
-        if (year % 4 == 0) {
-            System.out.println(year + " bir artık yıldır");
-        } else if (year % 400 == 0) {
-            System.out.println(year + " bir artık yıldır.");
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+            System.out.println(year + " bir artık yıldır");;
         } else
             System.out.println(year + " bir artık yıl değildir.");
 
